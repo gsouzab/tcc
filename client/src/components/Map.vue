0a7650ada308d51829061d5e6031aeadead3784e
Gabriel Bustamante <ext-gabriel.bustamante@imusica.com.br>
2018-04-05 11:09:36 -0300

    Adicionando botões FAB

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,64 +1,49 @@
 <template>
-  <gmap-map
-    :center="center"
-    :zoom="15"
-    style="width: 100%; height: calc(100% - 64px); position: absolute; left:0;">
+  <v-container fluid grid-list-md>
+    <gmap-map
+      :center="center"
+      :zoom="15"
+      style="width: 100%; height: calc(100% - 64px); position: absolute; left:0;">
 
-    <gmap-marker
-      :key="index"
-      v-for="(m, index) in markers"
-      :position="m.position"
-      :clickable="true"
-      @click="center=m.position">
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        @click="center=m.position">
 
-    </gmap-marker>
+      </gmap-marker>
+
+    </gmap-map>
     <v-speed-dial
-      v-model="fab"
-      :top="top"
-      :bottom="bottom"
-      :right="right"
-      :left="left"
-      :direction="direction"
-      :open-on-hover="hover"
-      :transition="transition"
-    >
+      :bottom=true
+      :right=true
+      :open-on-hover=true>
       <v-btn
         slot="activator"
         color="blue darken-2"
         dark
         fab
-        hover
-        v-model="fab"
-      >
-        <v-icon>account_circle</v-icon>
+        hover>
+        <v-icon>add</v-icon>
         <v-icon>close</v-icon>
       </v-btn>
       <v-btn
         fab
         dark
         small
-        color="green"
-      >
-        <v-icon>edit</v-icon>
+        color="green">
+        <v-icon>settings_input_antenna</v-icon>
       </v-btn>
       <v-btn
         fab
         dark
         small
-        color="indigo"
-      >
-        <v-icon>add</v-icon>
-      </v-btn>
-      <v-btn
-        fab
-        dark
-        small
-        color="red"
-      >
-        <v-icon>delete</v-icon>
+        color="indigo">
+        <v-icon>layers</v-icon>
       </v-btn>
     </v-speed-dial>
-  </gmap-map>
+  </v-container>
 </template>
 
 <script>
@@ -67,20 +52,9 @@ import SensorForm from "@/components/forms/SensorForm";
 export default {
   data() {
     return {
-      fab: false,
       center: { lat: -22.8617784, lng: -43.2296038 },
       markers: [],
       sensors: [],
-      direction: "top",
-      fab: false,
-      fling: false,
-      hover: false,
-      tabs: null,
-      top: false,
-      right: true,
-      bottom: true,
-      left: false,
-      transition: "slide-y-reverse-transition"
     };
   },
   methods: {
@@ -96,37 +70,19 @@ export default {
        */
     }
   },
-  computed: {
-    activeFab() {
-      switch (this.tabs) {
-        case "one":
-          return { class: "purple", icon: "account_circle" };
-        case "two":
-          return { class: "red", icon: "edit" };
-        case "three":
-          return { class: "green", icon: "keyboard_arrow_up" };
-        default:
-          return {};
-      }
-    }
-  },
-
-  watch: {
-    top(val) {
-      this.bottom = !val;
-    },
-    right(val) {
-      this.left = !val;
-    },
-    bottom(val) {
-      this.top = !val;
-    },
-    left(val) {
-      this.right = !val;
-    }
-  },
   components: {
     SensorForm
   }
 };
 </script>
+
+<style scoped>
+.speed-dial {
+  position: absolute;
+  right: 48px;
+}
+
+.btn--floating {
+  position: relative;
+}
+</style>
