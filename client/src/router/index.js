79fff52ad92d29820e32c346531b5da2db98a309
Gabriel Bustamante <ext-gabriel.bustamante@imusica.com.br>
2018-04-19 17:32:50 -0300

    Adicionando plugin de validação

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,10 +3,11 @@ import 'babel-polyfill';
 import Vue from 'vue';
 import Router from 'vue-router';
 import Vuetify from 'vuetify';
+import VeeValidate from 'vee-validate';
+import Map from '@/components/Map';
 import * as VueGoogleMaps from 'vue2-google-maps';
 
-import Map from '@/components/Map';
-
+Vue.use(VeeValidate);
 Vue.use(Router);
 Vue.use(Vuetify);
 Vue.use(VueGoogleMaps, {
