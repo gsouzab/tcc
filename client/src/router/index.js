47b20fcdd64714fc911667ac8e67d941e1dd5b98
gsouzab <gabriel@lancotec.com.br>
2018-08-26 21:13:35 -0300

    Refactor padão eslint

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,24 +4,35 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Vuetify from 'vuetify';
 import VeeValidate from 'vee-validate';
-import Map from '@/components/Map';
 import * as VueGoogleMaps from 'vue2-google-maps';
+import GmapCluster from 'vue2-google-maps/dist/components/cluster'; // replace src with dist if you have Babel issues
+
+import Map from '@/components/Map';
+import Dashboard from '@/components/Dashboard';
 
 Vue.use(VeeValidate);
 Vue.use(Router);
 Vue.use(Vuetify);
+
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyBUyTr9f-xkXsS4pMsww0ICCK1O-OR33sg',
+    key: 'AIzaSyAzCpPloIvDqI0U2rUtjIiRPZ6s0bD6U94',
   },
 });
+
+Vue.component('GmapCluster', GmapCluster);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Map',
+      name: 'map',
       component: Map,
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
     },
   ],
 });
