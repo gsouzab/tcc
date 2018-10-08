373de4be2176198460beb98242e4b5682a0b5901
gsouzab <gabriel@lancotec.com.br>
2018-10-08 10:21:08 -0300

    Kubernetes config

--- scripts/telemetry_publisher.py
+++ scripts/telemetry_publisher.py
@@ -41,7 +41,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+client.connect("35.198.28.153", 1883, 60)
 client.loop_start()
 
 while True:
