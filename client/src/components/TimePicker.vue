830d5811ee0e481455692ccdd023e69acfc8d316
Gabriel Bustamante <ext-gabriel.bustamante@imusica.com.br>
2018-10-17 17:18:37 -0300

    DateTime picker

--- client/src/components/TimePicker.vue
+++ client/src/components/TimePicker.vue
@@ -9,9 +9,9 @@
       <v-card>
         <v-card-text>
           <v-layout row justify-end>
-            <v-date-picker v-model="startDatePicker" no-title reactive locale="pt-br"></v-date-picker>
+            <v-date-picker v-model="startDatePicker" no-title reactive locale="pt-br" :events="selectedStartRange"></v-date-picker>
 
-            <v-date-picker v-model="endDatePicker" no-title reactive locale="pt-br"></v-date-picker>
+            <v-date-picker v-model="endDatePicker" no-title reactive locale="pt-br" :events="selectedEndRange" :min="minEndDate"></v-date-picker>
           </v-layout>
 
         </v-card-text>
@@ -28,8 +28,21 @@ export default {
     return {
       selectedTimeText: 'últimos 15 minutos',
       startDatePicker: null,
-      endDatePicker: null
+      endDatePicker: null,
+      selectedStartTime: null,
+      selectedEndTime: null,
+      minEndDate: null
     }
+  },
+  methods: {
+    selectedStartRange(date) {
+      const [,, day] = date.split('-')
+      return parseInt(day, 10) % 3 === 0
+    },
+    selectedEndRange(date) {
+      const [,, day] = date.split('-')
+      return parseInt(day, 10) % 3 === 0
+    },
   }
 }
 </script>
